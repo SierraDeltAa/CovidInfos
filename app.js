5127a1e4ad38abbb7370ca222b98c2558f7a06e5
SierraDeltAa <denil.merad@gmail.com>
2021-03-28 09:19:34 +0200

    port corrections

--- app.js
+++ app.js
@@ -19,5 +19,4 @@ app.use(routes);
 
 // Server start
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>console.log("Server is running on port", PORT));
+app.listen(process.env.PORT || 3000);
